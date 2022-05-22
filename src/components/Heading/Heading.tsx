b46de1200682f1c078b8ed596d3561eb6713043e
Carlos Di Matteo <carlosdima97@gmail.com>
2022-05-21 22:20:45 -0500

    input styling and heading comp

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,5 @@
+import { StyledHeading } from './Heading.styles';
+
+export function Heading({ children }: { children: any }) {
+	return <StyledHeading>{children}</StyledHeading>;
+}
--- src/components/Input/Input.styles.tsx
+++ src/components/Input/Input.styles.tsx
@@ -1,3 +1,17 @@
 import styled from 'styled-components';
 
-export const StyledInput = styled.input``;
+export const StyledInput = styled.input`
+	border-radius: ${({ theme: { borderRadius } }) => borderRadius.small};
+	padding: 1rem;
+	background: white;
+	font-size: 1rem;
+	line-height: 1.5rem;
+	border: none;
+	box-sizing: border-box;
+	&:active {
+		border: none;
+	}
+	margin-bottom: 6px;
+	box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+		0 10px 10px -5px rgba(0, 0, 0, 0.04);
+`;
